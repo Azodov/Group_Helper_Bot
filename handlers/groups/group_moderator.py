dc8fd6134d8a45c77f5e12e3b9ce50803bc9bcbc
DoniK <botirxonn@gmail.com>
2022-05-11 04:44:07 +0500

    Deploy Heroku

--- handlers/groups/group_moderator.py
+++ handlers/groups/group_moderator.py
@@ -67,6 +67,7 @@ async def read_only_mode(message: types.Message):
     try:
         await message.chat.restrict(user_id=member_id, can_send_messages=False, until_date=until_date)
         await message.reply_to_message.delete()
+        await message.delete()
     except aiogram.utils.exceptions.BadRequest as err:
         await message.answer(f"Xatolik! {err.args}")
         return
@@ -75,11 +76,11 @@ async def read_only_mode(message: types.Message):
     await message.answer(f"Foydalanuvchi {message.reply_to_message.from_user.full_name} {time} minut yozish huquqidan mahrum qilindi.\n"
                          f"Sabab: \n<b>{comment}</b>")
 
-    service_message = await message.reply("Xabar 5 sekunddan so'ng o'chib ketadi.")
-    # 5 sekun kutib xabarlarni o'chirib tashlaymiz
-    await asyncio.sleep(5)
-    await message.delete()
-    await service_message.delete()
+    # service_message = await message.reply("Xabar 5 sekunddan so'ng o'chib ketadi.")
+    # # 5 sekun kutib xabarlarni o'chirib tashlaymiz
+    # await asyncio.sleep(0)
+    # await message.delete()
+    # await service_message.delete()
 
 # read-only holatdan qayta tiklaymiz
 @dp.message_handler(IsGroup(), Command("unro", prefixes="!/"), AdminFilter())
